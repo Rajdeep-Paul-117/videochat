1ecb9d72e36c7acaf60712434505670d402db11c
Rajdeep Paul <rajdeeppaulact@gmail.com>
2021-05-26 20:38:35 +0530

    connection set

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -3,8 +3,8 @@ import {io} from 'socket.io-client'
 import Peer from 'simple-peer'
 
 const SocketContext=createContext();
-const socket=io('http://localhost:5000')
-
+/*const socket=io('http://localhost:5000')*/
+const socket=io('https://video-calling-website.herokuapp.com')
 
 const ContextProvider=({children})=>{
 
